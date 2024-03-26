a6ec7efa75d40aee73ad58c0ba8228789722f176
Dolgorykij <dolgo-rykij@mail.ru>
2024-03-26 20:22:52 +0300

    Added output with two languages

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Обязательно писать Hello World?Изменения!");
-
+        System.out.println("Обязательно писать Hello World?Изменения!");
+        System.out.println("Is it necessary to say Hello world?");
     }
 }
